fb555ad8a2f3b5d279b9a5b71ced19daa4df9f61
ubinjeon <156033838+ubinjeon@users.noreply.github.com>
2024-01-15 16:49:28 +0900

    Add files via upload

---
+++ m1_programming/cal4.py
@@ -0,0 +1,9 @@
+
+if __name__ == "__main__":
+    cal4(10,5)
+    
+def cal4(num1,num2):
+    print(f'{num1} + {num2} = {num1+num2}')
+    print(f'{num1} - {num2} = {num1-num2}')
+    print(f'{num1} * {num2} = {num1*num2}')
+    print(f'{num1} / {num2} = {num1/num2}') 
